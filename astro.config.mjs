ca63978d92664c08e2bdc73d0f2716287fc9fcc8
Patrick Barnhardt <pbarnhardt@helicalgames.net>
2026-02-24 22:42:57 -0500

    Update Astro configuration and add index page for MyOshi Theme Lab

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ export default defineConfig({
     integrations: [
 		starlight({
             title: 'MyOshi Theme Lab - by CrimsonStrife',
+            disable404Route: true,
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/crimsonstrife/oshi-lab' }],
             sidebar: [
                 { label: 'Theme Lab', link: '/lab/' },
