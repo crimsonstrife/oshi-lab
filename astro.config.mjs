b360dce1cfed6e1fbebb0d463a5a7405cd463a77
Patrick Barnhardt <pbarnhardt@helicalgames.net>
2026-03-01 17:14:53 -0500

    Add sitemap integration and robots.txt generation; update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+import markdoc from '@astrojs/markdoc';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://oshi-lab.app',
-    integrations: [mdx()],
+    integrations: [mdx(), sitemap({
+            filter: (page) =>
+                page !== 'https://oshi-lab.app/lab/preview' &&
+                page !== 'https://oshi-lab.app/error',
+        lastmod: new Date(),
+    }), markdoc()],
 });
